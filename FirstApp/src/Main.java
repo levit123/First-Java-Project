8ed7e3b9077c6529475948a163b029402d64ecc5
levit123 <LeviCaseBlodgett@gmail.com>
2022-06-03 13:43:35 -0700

    added Main class
    -added "Main.java" file, which contains the "Main" class that's initially run upon launching the project
    -added a couple println statements to the Main class to display console output

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from... you get the picture");
+    }
+}
